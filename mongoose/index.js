119359a87253bd8d3ff3bc51fcc2eeca44028980
funtuan <hank.chen@ohbottech.com>
2021-06-01 21:28:37 +0800

    remove bull

--- mongoose/index.js
+++ mongoose/index.js
@@ -1,4 +1,28 @@
 import mongoose from 'mongoose'
 import { mongo } from '../config'
 
-mongoose.connect(mongo.url)
+const db = mongoose.connection;
+
+db.on('connecting', function() {
+  console.log('connecting to MongoDB...');
+});
+
+db.on('error', function(error) {
+  console.error('Error in MongoDb connection: ' + error);
+  mongoose.disconnect();
+});
+db.on('connected', function() {
+  console.log('MongoDB connected!');
+});
+db.once('open', function() {
+  console.log('MongoDB connection opened!');
+});
+db.on('reconnected', function () {
+  console.log('MongoDB reconnected!');
+});
+db.on('disconnected', function() {
+  console.log('MongoDB disconnected!');
+  mongoose.connect(mongo.url, {server:{auto_reconnect:true}});
+});
+
+mongoose.connect(mongo.url, {server:{auto_reconnect:true}});
